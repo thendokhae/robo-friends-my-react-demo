e88ce64c610f8c5377f510016b5c2023f014c96a
B2B\MakahaneT <thendo.makahane@reedbusiness.com>
2018-08-03 12:49:14 +0200

    added redux thunk for async calls

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleWare from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 import registerServiceWorker from "./registerServiceWorker";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,39 @@
-import { CHANGE_SEARCH_FIELD } from "./constants";
-const initialState = {
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS
+} from "./constants";
+const initialStateSearch = {
   searchField: ""
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ""
+};
+
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {
+        isPending: false,
+        robots: action.payload
+      });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, { isPending: false });
     default:
       return state;
   }
